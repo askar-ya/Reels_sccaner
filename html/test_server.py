30b4052f3e1d4d34b0e309988132d7d692263e09
askar-ya <askaryaparov@yandex.ru>
2024-12-21 03:04:40 +0300

    fix

--- html/test_server.py
+++ html/test_server.py
@@ -10,5 +10,10 @@ def head():
 def reels():
     return render_template('reels.html')
 
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=True)
